8f4c246a5d280414edad15842043eb8f7f26375d
Arnab Das Utsa <144443522+iUtsa@users.noreply.github.com>
2025-01-24 21:17:57 +0000

    tree login v0.1

--- static/script.js
+++ static/script.js
@@ -141,3 +141,17 @@ function animate() {
 
 animate();
 
+
+
+//tree------
+
+document.addEventListener("DOMContentLoaded", () => {
+    const togglers = document.querySelectorAll(".caret");
+
+    togglers.forEach(toggler => {
+        toggler.addEventListener("click", function () {
+            this.parentElement.querySelector(".nested").classList.toggle("active");
+            this.classList.toggle("caret-down");
+        });
+    });
+});
